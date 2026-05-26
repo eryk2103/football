abe1983fb6c28dcf1b22609ce9317b10e164b12d
Eryk <eryk9903@gmail.com>
2026-05-26 13:51:26 +0200

    create index view for teams

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Team
 
-# Create your views here.
+
+def index(request):
+    teams = Team.objects.select_related('country', 'stadium').order_by('name')
+    return render(request, 'teams/index.html', {'teams': teams})

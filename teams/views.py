82e46ab0e6b2d40582d718c22fb74702f299c437
Eryk <eryk9903@gmail.com>
2026-05-27 15:17:15 +0200

    Add pagination for teams index

--- teams/views.py
+++ teams/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.db.models import Q
 from .models import Team
 from competitions.models import Competition
@@ -16,8 +17,12 @@ def index(request):
             Q(away_matches__competition__competition_id=competition_id)
         ).distinct()
 
+    paginator = Paginator(teams, 40)
+    page = paginator.get_page(request.GET.get('page'))
+
     return render(request, 'teams/index.html', {
-        'teams': teams,
+        'teams': page,
+        'page': page,
         'competitions': competitions,
         'selected_competition': competition_id,
     })

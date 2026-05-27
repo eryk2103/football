ac064195bda60c7e29d233001bdae2dfedc13b07
Eryk <eryk9903@gmail.com>
2026-05-27 12:00:34 +0200

    update matches model

--- matches/models.py
+++ matches/models.py
@@ -9,6 +9,8 @@ class Match(models.Model):
         FINISHED = 'FINISHED', 'Finished'
         POSTPONED = 'POSTPONED', 'Postponed'
         TBC = 'TBC', 'TBC'
+        CANCELLED = 'CANCELLED', 'Cancelled'
+        SCHEDULED = 'SCHEDULED', 'Scheduled'
 
     home_team = models.ForeignKey('teams.Team', related_name="home_matches", on_delete=models.SET_NULL, null=True, blank=True)
     away_team = models.ForeignKey('teams.Team', related_name="away_matches", on_delete=models.SET_NULL, null=True, blank=True)
@@ -20,7 +22,6 @@ class Match(models.Model):
     stadium = models.ForeignKey('stadiums.Stadium', on_delete=models.SET_NULL, null=True, blank=True)
     datetime = models.DateTimeField()
     status = models.CharField(max_length=20, choices=Status)
-    leg = models.PositiveSmallIntegerField(null=True, blank=True) # 1 or 2
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

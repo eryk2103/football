51d48c9a07d845d0f0c0e05cd813192b03598899
Eryk <eryk9903@gmail.com>
2026-05-27 15:14:21 +0200

    add filter by competition for teams

--- teams/views.py
+++ teams/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Team
+from competitions.models import Competition
 
 
 def index(request):
+    competitions = Competition.objects.order_by('name')
+    competition_id = request.GET.get('competition')
+
     teams = Team.objects.select_related('country', 'stadium').order_by('name')
-    return render(request, 'teams/index.html', {'teams': teams})
+
+    if competition_id:
+        teams = teams.filter(
+            Q(home_matches__competition__competition_id=competition_id) |
+            Q(away_matches__competition__competition_id=competition_id)
+        ).distinct()
+
+    return render(request, 'teams/index.html', {
+        'teams': teams,
+        'competitions': competitions,
+        'selected_competition': competition_id,
+    })
